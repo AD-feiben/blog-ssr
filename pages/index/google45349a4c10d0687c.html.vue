d47944384b34a2ac4f9137c7bf57c7ad4e1bd1ad
shirunpei <feiben.dev@gmail.com>
2018-08-07 00:37:06 +0800

    添加谷歌验证文件

---
+++ pages/index/google45349a4c10d0687c.html.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <div>
+    google-site-verification: google45349a4c10d0687c.html
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="less">
+</style>
