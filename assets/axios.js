3c07d970f986c5c3b97bdb38507251832c4e20ed
feiben <feiben.dev@gmail.com>
2018-09-14 15:58:19 +0800

    add api comment

--- assets/axios.js
+++ assets/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import qs from 'qs'
 
-let baseURL = 'http://feiben.xyz/api'
+let baseURL = 'http://feiben.xyz/api' // develop in local use http://localhost:3000
 
 const instance = axios.create({baseURL})
 
